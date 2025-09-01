00615ea37d3c8b5c88a37c4a9893b7f54709d2b2
Patrick Bell <patrick@Patricks-Laptop.local>
2025-09-01 14:27:11 +0100

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,14 @@
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+   allow do
+    origins 'https://paypulse-finance.netlify.app'
+    resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+   end
+
+
    allow do
      origins "http://localhost:5173"
 
@@ -7,4 +17,5 @@
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
        credentials: true
    end
+   
  end
